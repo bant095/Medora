fec73b8c008daacaefc2c992e579859017025726
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-09-12 10:10:39 +0100

    I added home page and all sections in the home page of the frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         primary: '#1DBAC4',
         secondary: '#261D3A',
       },
+      gridTemplateColumns: {
+        auto: 'repeat(auto-fill, minmax(200px, 1fr))',
+      },
     },
   },
   plugins: [],
